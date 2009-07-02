d51dc24cd0d8a6f668e7e9d2382e8c6d5dc192b1
François Pinard <pinard@iro.umontreal.ca>
2009-07-01 20:34:07 -0400

    Web site

---
+++ web/notes/site.mk
@@ -0,0 +1,5 @@
+package_name = Edily
+long_package_name = "Edily and Midi tools"
+rootdir = $(HOME)/entretien/edily/web/notes
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
---
+++ web/site.mk
@@ -0,0 +1,11 @@
+package_name = Edily
+long_package_name = "Edily and Midi tools"
+rootdir = $(HOME)/entretien/edily/web
+margin_color = "\#ffb747"
+caption_color = "\#cccccc"
+
+SITE_ROOT = 1
+
+GOALS += archives
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
